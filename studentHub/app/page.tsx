59b22636be07b6a5eaa13cfea3b2de3b8124ac8b
yehya <muhammedyehya60@gmail.com>
2024-10-30 22:34:06 +0300

    first look of the registration and login pages

--- studentHub/app/page.tsx
+++ studentHub/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>home page</h1>
+      <h1 className="text-4xl text-blue-600">home page</h1>
+      <Link href="/registration" className="mr-5">register</Link>
+      <Link href="/login">login</Link>
     </>
   );
 }
